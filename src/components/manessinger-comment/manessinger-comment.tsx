8ac1edab3259c546bfbe125d3cf3a41ac7e51d59
Andreas Manessinger <andreas.manessinger@wien.gv.at>
2018-08-26 00:47:00 +0200

    Can access the fragment of the window's location. Added website url field. Made ist possible to style portably into the component's shadow DOM via CSS variables.

--- src/components/manessinger-comment/manessinger-comment.tsx
+++ src/components/manessinger-comment/manessinger-comment.tsx
@@ -1,4 +1,4 @@
-import { Component, State } from '@stencil/core';
+import {Component, Prop, State} from '@stencil/core';
 
 @Component({
   tag: 'manessinger-comment',
@@ -9,13 +9,18 @@ export class ManessingerComment {
 
   @State() userNameValue: string;
   @State() emailValue: string;
+  @State() websiteValue: string;
   @State() commentValue: string;
+
+  @Prop({ context: 'window' }) private window: Window;
 
   handleSubmit(e) {
     e.preventDefault()
     console.log(this.userNameValue);
     console.log(this.emailValue);
+    console.log(this.websiteValue);
     console.log(this.commentValue);
+    console.log(this.window.location.hash.substr(1));
     // send data to our backend
   }
 
@@ -25,6 +30,10 @@ export class ManessingerComment {
 
   handleChangeEmail(event) {
     this.emailValue = event.target.value;
+  }
+
+  handleChangeWebsite(event) {
+    this.websiteValue = event.target.value;
   }
 
   handleChangeComment(event) {
@@ -41,6 +50,9 @@ export class ManessingerComment {
 
             <label id="email-label" htmlFor="email">Email (opt.):</label>
             <input id="email" type="text" value={this.emailValue} onInput={(event) => this.handleChangeEmail(event)} />
+
+            <label id="website-label" htmlFor="website">Website (opt.):</label>
+            <input id="website" type="text" value={this.websiteValue} onInput={(event) => this.handleChangeWebsite(event)} />
 
             <label id="comment-label" htmlFor="commment">Your comment:</label>
             <textarea id="comment" value={this.commentValue} onInput={(event) => this.handleChangeComment(event)} />
