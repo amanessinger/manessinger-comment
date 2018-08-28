28acecaa5e51202c9f0a16bc343c4261d1aea123
Andreas Manessinger <andreas.manessinger@wien.gv.at>
2018-08-28 23:50:07 +0200

    Already submitting, although with a clumsy fetch.

--- src/components/manessinger-comment/manessinger-comment.tsx
+++ src/components/manessinger-comment/manessinger-comment.tsx
@@ -15,13 +15,39 @@ export class ManessingerComment {
   @Prop({ context: 'window' }) private window: Window;
 
   handleSubmit(e) {
-    e.preventDefault()
-    console.log(this.userNameValue);
-    console.log(this.emailValue);
-    console.log(this.websiteValue);
-    console.log(this.commentValue);
-    console.log(this.window.location.hash.substr(1));
+    e.preventDefault();
+    let data = {
+      "attach_to": this.window.location.hash.substr(1),
+      "name": this.userNameValue,
+      "email": this.emailValue,
+      "website": this.websiteValue,
+      "comment": this.commentValue
+    };
     // send data to our backend
+    this.postData(data)
+      .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+      .catch(error => console.error(error));
+  }
+
+  postData(data, url = `https://7e6ex1bf2c.execute-api.us-east-2.amazonaws.com/default/manessingercomment_receiver`) {
+    // Default options are marked with *
+    return fetch(url, {
+      method: "POST",
+      mode: "no-cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      //credentials: "same-origin", // include, same-origin, *omit
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+      .then(response => response.json()); // parses response to JSON
+  }
+
+  handleCancel() {
+    this.window.open(this.window.location.toString(), '_self').close();
   }
 
   handleChangeUserName(event) {
@@ -57,8 +83,8 @@ export class ManessingerComment {
             <label id="comment-label" htmlFor="commment">Your comment:</label>
             <textarea id="comment" value={this.commentValue} onInput={(event) => this.handleChangeComment(event)} />
 
-            <input id="cancel" type="button" value="Cancel"/>
-            <input id="submit" type="submit" value="Submit" />
+            <input id="cancel" type="button" value="Cancel" onClick={() => this.handleCancel()}/>
+            <input id="submit" type="submit" value="Submit" onClick={(event) => this.handleSubmit(event)}/>
           </div>
         </form>
       </div>
